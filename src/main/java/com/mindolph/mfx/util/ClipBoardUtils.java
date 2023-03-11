88021fc70a808e67e47564a55e084d332f9c9e32
mindolph <mindolph.com@gmail.com>
2023-03-11 18:22:37 +0800

    file support to ClipBoardUtils

--- src/main/java/com/mindolph/mfx/util/ClipBoardUtils.java
+++ src/main/java/com/mindolph/mfx/util/ClipBoardUtils.java
@@ -2,9 +2,12 @@ package com.mindolph.mfx.util;
 
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+
+import java.io.File;
+import java.util.List;
 
 /**
- * 
  * @author allen
  */
 public class ClipBoardUtils {
@@ -27,10 +30,33 @@ public class ClipBoardUtils {
 
     /**
      * Retrieve text from system clipboard.
+     *
      * @return
      */
-    public static String textFromClipboard(){
+    public static String textFromClipboard() {
         Clipboard clipboard = Clipboard.getSystemClipboard();
         return clipboard.getString();
+    }
+
+    /**
+     * Push files to system clipboard.
+     *
+     * @param files
+     */
+    public static void filesToClipboard(List<File> files) {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent clipboardContent = new ClipboardContent();
+        clipboardContent.putFiles(files);
+        clipboard.setContent(clipboardContent);
+    }
+
+    /**
+     * Retrieve files from system clipboard.
+     * 
+     * @return
+     */
+    public static List<File> filesFromClipboard() {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        return clipboard.getFiles();
     }
 }
